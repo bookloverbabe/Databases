150e0a906d48d08f4fdbd668d49cce74b582dd91
bookloverbabe <solmaz@hotmail.co.uk>
2023-03-28 11:29:57 +0100

    phase one complete

--- phase_one_sql_bites/music_library.sql
+++ phase_one_sql_bites/music_library.sql
@@ -80,3 +80,55 @@ UPDATE albums
  SELECT id, title, release_year
    FROM albums
    WHERE release_year = 1972;
+
+-- Update the title and release_year of the album with ID 1 (in a single query). 
+-- Then use a SELECT query to get the list of albums and verify that one has been updated.
+UPDATE albums
+  SET release_year = 1972, title = 'new title'
+ WHERE id = 1;
+
+SELECT id, title, release_year
+  FROM albums
+  WHERE id = 1;
+
+-- Delete the album with ID 12. Then use a SELECT query to get the list of albums
+-- and verify that one has been deleted.
+DELETE FROM albums
+      WHERE id = 12
+
+SELECT id, title, release_year
+  FROM albums
+  WHERE id = 12;
+
+-- INSERT a new record in albums with title 'Mezzanine', and release year 1998.
+INSERT INTO albums (title, release_year)
+     VALUES ('Mezzanine', 1998);
+
+SELECT id, title, release_year
+  FROM albums
+  WHERE title = 'Mezzanine';
+
+-- We forgot to link this new record with the correct artist. 
+-- Use an UPDATE query to update the artist_id of that new album with Massive Attack 
+-- (it should have the id value 5).
+
+UPDATE albums
+  SET artist_id = 5
+WHERE id = 13;
+
+SELECT id, title, release_year, artist_id
+  FROM albums
+  WHERE title = 'Mezzanine';
+
+-- Insert a new artist of your choice, and a new album related to that artist 
+-- Then run a SELECT query to check the new artist is now in the table.
+
+INSERT INTO albums (title, release_year, artist_id)
+     VALUES ('OK Computer', 1997, 6);
+
+INSERT INTO artists (name, genre)
+     VALUES('Radiohead', 'alternative rock');
+
+SELECT id, title, release_year, artist_id
+  FROM albums
+WHERE title = 'OK Computer';
